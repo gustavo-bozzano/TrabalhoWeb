f18083f7461d2d24cc9530a89579bdefe71cf72d
Gustavo Bozzano <gustavocbozzano@gmail.com>
2023-06-29 21:07:46 -0300

    atualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../app/services/api.service.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: '/app.component.html',
+	styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'TrabalhoWeb';
+
+export class AppComponent implements OnInit {
+	public foto1: string = "";
+	public foto2: string = "";
+	public foto3: string = "";
+	public foto4: string = "";
+	public foto5: string = "";
+	public foto6: string = "";
+
+	constructor(private apiService: ApiService) { }
+
+	ngOnInit() {
+		setTimeout(async () => {
+		  this.foto1 = await this.getCao();
+		  this.foto2 = await this.getCao();
+		  this.foto3 = await this.getCao();
+		  this.foto4 = await this.getCao();
+		  this.foto5 = await this.getCao();
+		  this.foto6 = await this.getCao();
+		});
+	  }
+
+	public async getCao() {
+		try {
+			const data = await this.apiService.getCao().toPromise();
+			return data.message;
+		} catch (error: any) {
+			console.error('Ocorreu um erro ao pegar a imagem do cachorro', error);
+			return error.toString();
+		}
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
